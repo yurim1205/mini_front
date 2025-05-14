33373ea742cfc455acddfe941deaf9bc4b35d268
yurim <eoulim3237@naver.com>
2025-05-14 15:50:21 +0900

    feat: 버튼,파일 피드백 페이지 작업

--- frontend/src/pages/loading.jsx
+++ frontend/src/pages/loading.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import img1 from "../assets/img/owl.png";
 import img2 from "../assets/img/owl2.png";
@@ -6,13 +6,24 @@ import img3 from "../assets/img/owl3.png";
 import img4 from "../assets/img/owl4.png";
 import img5 from "../assets/img/owl5.png";
 import img6 from "../assets/img/owl6.png";
-import img7 from "../assets/img/owl.7png";
+import img7 from "../assets/img/owl7.png";
 // API가 나오면, setTimeout 대신 API 호출 코드로 수정하기
 
 function Loading() {
     const navigate = useNavigate();
+    const images = [img1, img2, img3, img4, img5, img6, img7];
+    const [currentImg, setCurrentImg] = useState(img1);
 
     useEffect(() => {
+        // 0.5초마다 랜덤 이미지 변경
+        const interval = setInterval(() => {
+          const randomIdx = Math.floor(Math.random() * images.length);
+          setCurrentImg(images[randomIdx]);
+        }, 900);
+        return () => clearInterval(interval);
+      }, [images]);
+
+      useEffect(() => {
         const timer = setTimeout(() => {
           navigate('/result');
         }, 20000);
@@ -31,10 +42,15 @@ function Loading() {
                   filter: "blur(48px)",
                 }}
               />
-              <span className="relative text-black text-sm font-extrabold z-10 text-center">
-                조금만 기다려주세요<br />음성을 분석하고 있어요
-              </span>
+              <img
+                src={currentImg}
+                alt="owl"
+                className="absolute z-20 w-24 h-24 object-contain left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+              />
             </div>
+            <span className="block mt-0 text-black text-sm font-extrabold text-center">
+              조금만 기다려주세요<br />음성을 분석하고 있어요
+            </span>
           </div>
         </div>
       );
